8348c9036914ac50e7d4f775d68ce6f4f0773a18
Fernando Agüero <fjaguero@gmail.com>
2018-12-19 22:42:24 +0100

    Init

---
+++ precache-manifest.18800abf89ad1a1a9e503683a4b48382.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b30701c2780d391c0045276c1209c8e",
+    "url": "/static/media/hat.4b30701c.png"
+  },
+  {
+    "revision": "fdd2c0938c39bf875d46",
+    "url": "/static/css/main.b5d0c998.chunk.css"
+  },
+  {
+    "revision": "16db2177cd3d296b5d3fdf5999c912a0",
+    "url": "/static/media/bg.16db2177.png"
+  },
+  {
+    "revision": "acf5de97c9b0a1c5f65c",
+    "url": "/static/js/1.acf5de97.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "421c8c7b4cdf42bb3efc15fa90f5c929",
+    "url": "/static/media/blank.421c8c7b.jpg"
+  },
+  {
+    "revision": "fdd2c0938c39bf875d46",
+    "url": "/static/js/main.fdd2c093.chunk.js"
+  },
+  {
+    "revision": "a18463b33a8ed8db66382eeb12ac8f59",
+    "url": "/static/media/balls.a18463b3.png"
+  },
+  {
+    "revision": "e99fb3d601771312d21adc46f598093a",
+    "url": "/static/media/bell.e99fb3d6.png"
+  },
+  {
+    "revision": "05855794846e7bb374d4450c7f129fc7",
+    "url": "/static/media/tree.05855794.png"
+  },
+  {
+    "revision": "66bfc60d39702cb82710e1d3c6912e35",
+    "url": "/static/media/header.66bfc60d.png"
+  },
+  {
+    "revision": "acf5de97c9b0a1c5f65c",
+    "url": "/static/css/1.3fd431bd.chunk.css"
+  },
+  {
+    "revision": "15b7bbf3319a96d1be61fe6bfb497731",
+    "url": "/index.html"
+  }
+];
